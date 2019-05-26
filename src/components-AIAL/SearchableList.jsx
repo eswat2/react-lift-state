691e6348dfc8503935ecae48e5e195872bf1be62
Richard Hess <eswat2@gmail.com>
2019-05-26 14:24:59 -0700

    shorten label for mobile

--- src/components-AIAL/SearchableList.jsx
+++ src/components-AIAL/SearchableList.jsx
@@ -24,7 +24,7 @@ const SearchableList = ({ className, list }) => {
   return (
     <div className={className}>
       <Search query={query} handleQuery={handleQuery}>
-        Search List:
+        Search:
       </Search>
       <hr />
       <List list={filteredList} />
