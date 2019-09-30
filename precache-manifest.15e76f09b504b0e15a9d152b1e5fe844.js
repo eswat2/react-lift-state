5a524559d694a9253633f2dbe2ab3b04aeb29bbd
Richard Hess <eswat2@gmail.com>
2019-09-29 20:30:28 -0700

    Updates

--- precache-manifest.15e76f09b504b0e15a9d152b1e5fe844.js
+++ precache-manifest.15e76f09b504b0e15a9d152b1e5fe844.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f9381d319ac63a80c917aa21535d7bbd",
+    "revision": "7b47301c88d241f1e8aa0adcfb9461d3",
     "url": "/react-lift-state/index.html"
   },
   {
-    "revision": "59152ef98035747a6b48",
+    "revision": "40c13719aaff163c268a",
     "url": "/react-lift-state/static/css/main.8c7f7a85.chunk.css"
   },
   {
-    "revision": "7eb93ab874010614ce30",
-    "url": "/react-lift-state/static/js/2.ffb9b6fb.chunk.js"
+    "revision": "90f8851d63ee161d69e3",
+    "url": "/react-lift-state/static/js/2.a01f5279.chunk.js"
   },
   {
-    "revision": "59152ef98035747a6b48",
-    "url": "/react-lift-state/static/js/main.51fe6a33.chunk.js"
+    "revision": "40c13719aaff163c268a",
+    "url": "/react-lift-state/static/js/main.e2fc44a4.chunk.js"
   },
   {
     "revision": "9801c673a95a3458b3ac",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-lift-state/precache-manifest.1a8baac5cb6dfa529686f3ce87ee820f.js"
+  "/react-lift-state/precache-manifest.15e76f09b504b0e15a9d152b1e5fe844.js"
 );
 
 self.addEventListener('message', (event) => {
